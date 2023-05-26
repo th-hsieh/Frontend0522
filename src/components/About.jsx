49ba34de52d04e32b4234d47cde007b15087f476
th-hsieh <t.hsuanhsieh@gmail.com>
2023-05-25 23:39:33 -0400

    modify the 'mission statement page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,46 @@
-const About  = () => {
+const About = () => {
     return (
-    <div className="row justify-content-center">
-        <div className="col-sm-12 text-center">
-            <h2> INFO</h2>
+      <div className="row justify-content-center">
+        <div className="col-sm-6">
+          <div className="d-flex flex-column align-items-center">
+            <h5 style={{ margin: '0em 1em 0em 2em' }}>INFO</h5>
+            <h6 style={{ margin: '0em 1em 2em 2em' }}>
+              As a female physicist and engineer who was born and educated in Taiwan, further studied and worked in over 8 countries, I know very well know gender inequality in STEM domain persists as a global problem. public+privacy emphasizes on providing an inclusive discussion forum, where each voice is equally displayed. This way, our next generations in STEM can have truly gender equality.
+            </h6>
+          </div>
         </div>
-
-        <div className="col-sm-12 text-center">
-            <h5> Mission Statement</h5>
+        <div className="col-sm-6">
+          <div className="d-flex flex-column align-items-center">
+            <h5 style={{ margin: '0em 2em 0em 1em'}}>Mission Statement</h5>
+            <h6 style={{ margin: '0em 2em 2em 1em'}}>
+              What is public, what is private? We started sharing our private experiences on health care, gender equality, and educational system publicly, so that the individuals who have the same experiences will know that you are not alone. public+privacy bears this mission in mind to serve as a channel for building a better environment for the next STEM generation.
+            </h6>
+          </div>
         </div>
-
-        <div className="col-sm-12 text-center">
-            <h5> Collaboration</h5>
+  
+        <div className="col-sm-6">
+          <div className="d-flex flex-column align-items-center">
+            <h5 style={{ margin: '0em 1em 0em 2em'}}>Collaboration</h5>
+            <h6 style={{ margin: '0em 1em 2em 2em' }}>Come Join as Collaborators, and share your experiences!</h6>
+          </div>
         </div>
-
-        <div className="col-sm-12 text-center">
-            <h5> Contact</h5>
+  
+        <div className="col-sm-6">
+          <div className="d-flex flex-column align-items-center">
+            <h5 style={{ margin: '0em 2em 0em 1em'}}>Contact</h5>
+            <h6 style={{ margin: '0em 1em 2em 2em' }}>t.hsuanhsieh@gmail.com</h6>
+          </div>
         </div>
-
-        <div className="col-sm-12 text-center">
-            <h5> Selected Essays</h5>
+  
+        <div className="col-sm-6">
+          <div className="d-flex flex-column align-items-center">
+            <h5 style={{ margin: '0em 2em 0em 0em'}}>Selected Essays</h5>
+            <h6 style={{ margin: '0em 2em 0em 0em'}}>Related Research Essays from NGO, along with my own publications will be updated bi-weekly.</h6>
+          </div>
         </div>
-    </div>
+      </div>
     );
-}
- 
-export default About ;
+  };
+  
+  export default About;
+  
