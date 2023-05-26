2f458fc8032b3959081f994c753604d03cebad97
th-hsieh <t.hsuanhsieh@gmail.com>
2023-05-26 04:17:38 -0400

    adjust styles

--- src/components/OpinionsList.js
+++ src/components/OpinionsList.js
@@ -22,34 +22,39 @@ const OpinionsList = () => {
     },[]);
 
     return (
-        <div className="row justify-content-center">
-            <div className="col-sm-12 text-center">
+      <div className="container">
+
+          <div className="row justify-content-center">
+            <div className="col-sm-6 text-center">
                 <h2>List of Opinions</h2>
             </div>
-          
-            <div className="row justify-content-center">
-                <div className="mt-3">
-                  {
-                    opinions.length > 0 ? (
-                      opinions.map(opinion => (
-                        <div key={opinion.id} className="mt-3">  
-                        {/* opinions-preview */}
-                          <Link to={`/forum/opinions/${opinion.id}`} className="text-decoration-none">
-                            <h5 className="text-capitalize" style={{backgroundColor: "black",color: "white",borderRadius: "5px"}}>{opinion.title}</h5> 
-                          </Link>
-                          <h6 className="text-capitalize">{opinion.category}</h6>
-                          <Moment fromNow style={{ fontStyle: 'italic' }}>{opinion.updatedAt}</Moment>
-                        </div>
-                      ))
-                    ) : (
-                      <div className="text-center">
-                        <h5>No opinions available</h5>
-                    </div>
-                    )
-                  }
-                </div>
+          </div>
+
+          <div className="row justify-content-center">
+            <div className="col-sm-6 text-center">
+              <div className="mb-3 d-flex flex-column align-items-center" style={{ width: '100%', margin: '0 auto' }}>
+              {
+                opinions.length > 0 ? (
+                  opinions.map(opinion => (
+                  <div key={opinion.id} className="mb-3">  
+                    <Link to={`/forum/opinions/${opinion.id}`} className="text-decoration-none">
+                      <h5 className="text-capitalize" style={{ backgroundColor: "gray",color: "white",borderRadius: "5px", margin: '0em 0em 0em 0em'}}>{opinion.title}</h5> 
+                      <h6 className="text-capitalize text-center">{opinion.category}</h6>
+                    <Moment className="text-center" fromNow style={{ fontStyle: 'italic' }}>{opinion.updatedAt}</Moment>
+                    </Link>
+                  </div>
+                  ))
+                  ) : (
+                  <div className="text-center">
+                    <h5>No opinions available</h5>
+                  </div>
+                )
+              }
               </div>
             </div>
+          </div>
+      </div>
+   
         );
 }
 export default OpinionsList;       
